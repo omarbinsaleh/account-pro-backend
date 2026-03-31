0f0bff8f658427cf7131758b0423af40641a6f99
omarbinsaleh <omarbinsaleh44@gmail.com>
2026-04-01 01:07:34 +0400

    implemented the company model

---
+++ src/models/Company.js
@@ -0,0 +1,45 @@
+const mongoose = require('mongoose');
+
+const companySchema = new mongoose.Schema({
+   name: {
+      type: String,
+      required: [true, 'Company name is required'],
+      trim: true
+   },
+   email: {
+      type: String,
+      required: [true, 'Company email is required'],
+      minlength: [5, 'Email must be at least 5 characters long'],
+      unique: [true, 'Company with this email already exists'],
+      trim: true,
+      lowercase: true,
+      match: [/\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/, 'Please use a valid email address.']
+   },
+   phone: {
+      type: String,
+      required: [true, 'Company phone number is required'],
+      match: [/^\+?[1-9]\d{1,14}$/, 'Please use a valid phone number'],
+   },
+   address: {
+      type: String,
+      required: [true, 'Company address is required'],
+      trim: true,
+   },
+   website: String,
+   description: String,
+   logo: String,
+   currency: {
+      type: String,
+      trim: true,
+      minlength: [3, 'Currency must be at least 3 characters long'],
+      maxlength: [3, 'Currency must be at most 3 characters long'],
+      uppercase: true,
+      default: 'USD',
+   },
+}, { timestamps: true });
+
+// Define the Company Model
+const Company = mongoose.model('Company', companySchema);
+
+// exports the company model
+module.exports = Company;
