90c6fa420deef7b1ebc322643736a7486ca656fb
omarbinsaleh <omarbinsaleh44@gmail.com>
2026-05-01 02:22:41 +0400

    modified the createCompany controller

--- src/controllers/companyController.js
+++ src/controllers/companyController.js
@@ -13,44 +13,51 @@ const companyController = {};
 // @middleware: authUser
 // @description: Create a new company
 companyController.createCompany = async (req, res) => {
+   // Step 01: Get the user ID from the request object
+   const userId = req.user?._id;
+   if (!userId || !mongoose.isValidObjectId(userId)) {
+      return res.status(400).json({ success: false, message: 'Missing or invalid user ID', data: null });
+   };
+
+   // Step 02: Extract company details from the request body
+   const { name, email, phone, address, website, description, logo, currency } = req.body;
+
+   // Step 03: Validate the required fields
+   if (!name || typeof name !== 'string' || !name.trim().length) {
+      return res.status(400).json({ success: false, message: 'Company name is required and must be a non-empty string', data: null });
+   };
+
+   // Step 04: Validate the email field
+   if (!email || typeof email !== 'string' || !email.trim().length < 5 || !email.includes('@')) {
+      return res.status(400).json({ success: false, message: 'Company email is required and must be a valid email address', data: null });
+   };
+
+   // Step 05: Validate the phone number field
+   if (!phone || typeof phone !== 'string' || !phone.trim().length) {
+      return res.status(400).json({ success: false, message: 'Company phone number is required and must be a valid phone number', data: null });
+   };
+
+   // Step 06: Validate the address field
+   if (!address || typeof address !== 'string' || !address.trim().length) {
+      return res.status(400).json({ success: false, message: 'Company address is required and must be a non-empty string', data: null });
+   };
+
+   // Step 07: Start a new session for the transaction to ensure atomicity of the company creattion and membership creation operations
+   const session = await mongoose.startSession();
+   
    try {
-      // Step 01: Get the user ID from the request object
-      const userId = req.user?._id;
-      if (!userId || !mongoose.isValidObjectId(userId)) {
-         return res.status(400).json({ success: false, message: 'Missing or invalid user ID', data: null });
+      // Step 08: Check if the Company email already exists
+      const existingCompany = await Company.findOne({ email: email.trim().toLowerCase() });
+      if (existingCompany) {
+         session.endSession();
+         return res.status(400).json({ success: false, message: 'A company with this email already exists', data: null });
       };
 
-      // Step 02: Check if the user exists
-      const user = await User.findById(userId);
-      if (!user) {
-         return res.status(404).json({ success: false, message: 'User not found', data: null });
-      };
+      // Step 09: Start the transaction for creating the company and the membership
+      session.startTransaction();
 
-      // Step 03: Extract company details from the request body
-      const { name, email, phone, address, website, description, logo, currency } = req.body;
-
-      // Step 04: Validate the required fields
-      if (!name || typeof name !== 'string' || !name.trim().length) {
-         return res.status(400).json({ success: false, message: 'Company name is required and must be a non-empty string', data: null });
-      };
-
-      // Step 05: Validate the email field
-      if (!email || typeof email !== 'string' || !email.trim().length < 5 || !email.includes('@')) {
-         return res.status(400).json({ success: false, message: 'Company email is required and must be a valid email address', data: null });
-      };
-
-      // Step 06: Validate the phone number field
-      if (!phone || typeof phone !== 'string' || !phone.trim().length) {
-         return res.status(400).json({ success: false, message: 'Company phone number is required and must be a valid phone number', data: null });
-      };
-
-      // Step 07: Validate the address field
-      if (!address || typeof address !== 'string' || !address.trim().length) {
-         return res.status(400).json({ success: false, message: 'Company address is required and must be a non-empty string', data: null });
-      };
-
-      // Step 08: Create a new company document
-      const newCompany = new Company({
+      // Step 10: Create a new company document
+      const companyArray = await Company.create([{
          name,
          email,
          phone,
@@ -60,23 +67,34 @@ companyController.createCompany = async (req, res) => {
          currency,
          owner: userId,
          logo
-      });
+      }], { session });
+      const newCompany = companyArray[0];
 
-      // Step 09: Create a new membership document for the company owner
-      const newMembership = await Membership.create({
+      // Step 11: Create a new membership document for the company owner
+      await Membership.create([{
          userId,
          companyId: newCompany._id,
          role: 'owner'
-      });
+      }], { session });
 
-      // Step 10: Save the new company document to the database
-      await newCompany.save();
+      // Step 12: Commit the transaction and end the session
+      await session.commitTransaction();
+      await session.endSession();
 
-      // Step 11: Return a success response with the created company data
+      // Step 13: Return a success response with the created company data
       return res.status(201).json({ success: true, message: 'Company created successfully', data: newCompany });
    } catch (error) {
+      // Step 14: Rollback the transaction
+      if (session.inTransaction()) {
+         await session.abortTransaction();
+      }
+      
+      // Step 15: End the session
+      await session.endSession();
+
+      // Step 16: Return an error response with a generic message to avoid exposing sensitive error details
       return res.status(500).json({ success: false, message: 'An error occurred while creating the company, please try again later', data: null });
-   };
+   }
 };
 
 // @name: findCompanyById 
@@ -84,28 +102,28 @@ companyController.createCompany = async (req, res) => {
 // @access: Private
 // @middleware: authUser
 // @description: Find a company by it's ID
-companyController.findCompanyById = async (req, res) => {};
+companyController.findCompanyById = async (req, res) => { };
 
 // @name: findCompanies 
 // @path: GET /api/companies
 // @access: Private
 // @middleware: authUser
 // @description: Find many companies
-companyController.findCompanies = async (req, res) => {};
+companyController.findCompanies = async (req, res) => { };
 
 // @name: updateCompanyById
 // @path: PATCH /api/companies/:id
 // @access: Private
 // @middleware: authUser
 // @description: Update a company information by it's ID
-companyController.updateCompanyById = async (req, res) => {};
+companyController.updateCompanyById = async (req, res) => { };
 
 // @name: deleteCompanyById
 // @path: DELETE /api/companies/:id
 // @access: Private
 // @middleware: authUser
 // @description: Delete a company by it's ID
-companyController.deleteCompanyById = async (req, res) => {};
+companyController.deleteCompanyById = async (req, res) => { };
 
 // Export the company controller
 module.exports = companyController;
